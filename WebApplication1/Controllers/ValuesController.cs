9f90f615f59dd145e179544d3857fca5fd29591a
Oscar.Chen <oscar.chen@medici-software.com>
2015-09-10 15:29:36 +0800

    test controller post method handle with Custom Entity

--- WebApplication1/Controllers/ValuesController.cs
+++ WebApplication1/Controllers/ValuesController.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Mvc;
 
 namespace WebApplication1.Controllers
 {
@@ -15,5 +16,29 @@ namespace WebApplication1.Controllers
                 "values 2"
             };
         } 
+    }
+
+
+    public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [System.Web.Mvc.HttpPost]
+        public ActionResult SaveNecklaceAnnotaion([Bind(Prefix = "NecklaceAnnotationDto")]NecklaceAnnotationDto annotation)
+        {
+            if (ModelState.IsValid)
+            {
+                annotation.NecklaceId = 999;
+            }
+            return View(annotation);
+        }
+    }
+
+    public class NecklaceAnnotationDto
+    {
+        public int NecklaceId { get; set; }
+        public string Annotation { get; set; }
     }
 }
