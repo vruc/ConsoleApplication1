eee34a7b52515171019015c5eb6484f5e4c03ee3
Oscar.Chen <chanruida@outlook.com>
2015-09-23 15:29:02 +0000

    test new exception handler

---
+++ WebApplication3/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication3.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            throw new ArgumentNullException("Exception");
+            //return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]Latlnt value)
+        {
+            throw new ArgumentNullException("value");
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]Latlnt value)
+        {
+            throw new ArgumentNullException("value");
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class Latlnt
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
---
+++ WebApplication3/Global.asax.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Filters;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebApplication3
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            GlobalConfiguration.Configuration.MessageHandlers.Add(new ExceptionHandler());
+        }
+    }
+
+
+    public class ExceptionHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+
+            var headers = request.ToString();
+            var body = request.Content.ReadAsStringAsync().Result;
+            var fullRequest = headers + "\r\n\r\n" + body + "\r\n\r\n";
+
+            return base.SendAsync(request, cancellationToken)
+                .ContinueWith(task =>
+                {
+                    var responseHeaders = task.Result.ToString();
+                    var responseBody = task.Result.Content.ReadAsStringAsync().Result;
+
+                    var fullResponse = responseHeaders + "\r\n\r\n" + responseBody;
+
+                    var response = task.Result;
+
+                    response.Content = new StringContent(fullRequest + fullResponse);
+                    return response;
+                }, cancellationToken);
+
+        }
+    }
+
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public class CustomExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        {
+            var message = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+
+            message.Content = new StringContent(
+                string.Format("{0} \r\n {1}", actionExecutedContext.Exception.Message,
+                    actionExecutedContext.Exception.StackTrace));
+
+            actionExecutedContext.Response = message;
+
+            base.OnException(actionExecutedContext);
+        }
+    }
+
+}
